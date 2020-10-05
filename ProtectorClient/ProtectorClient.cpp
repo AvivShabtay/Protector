4adbbebf77961f62bf597855db39ed3f9f1d1c30
Aviv Shbaty <shabtay.aviv@gmail.com>
2020-10-05 17:36:06 +0300

    Add command line parser
    * feat: Add helper function to parse command line arguments
    * feat: Add helper function to print application usage

--- ProtectorClient/ProtectorClient.cpp
+++ ProtectorClient/ProtectorClient.cpp
@@ -7,6 +7,8 @@ bool g_DeviceCreated = false;
 
 // Define prototypes:
 int Error(const char* text);
+bool ParseCommandLine(std::pair<std::wstring, std::wstring>& userInput, int argc, wchar_t** argv);
+void ShowUsage(std::wstring appName);
 
 int main() {
 
@@ -53,4 +55,73 @@ int Error(const char* text) {
 		CloseHandle(g_hProtectorDevice);
 
 	return 1;
+}
+
+/*
+* Helper function to parse the user command line to the application
+* specific use cases.
+*/
+bool ParseCommandLine(std::pair<std::wstring, std::wstring>& input, int argc, wchar_t** argv) {
+	if (argc <= 1) {
+		ShowUsage(argv[0]);
+		return false;
+	}
+
+	for (int i = 1; i < argc; i++) {
+		std::wstring arg = argv[i];
+
+		if ((arg == L"-h") || (arg == L"--help")) {
+			ShowUsage(argv[0]);
+			return false;
+		}
+
+		if ((arg == L"-a") || (arg == L"--append")) {
+			if (i + 1 < argc) {
+				input.first = arg;
+				input.second = argv[i + 1];
+				return true;
+			}
+			else {
+				std::wcerr << L"--append option requires <path>." << std::endl;
+			}
+		}
+
+		if ((arg == L"-r") || (arg == L"--remove")) {
+			if (i + 1 < argc) {
+				input.first = arg;
+				input.second = argv[i + 1];
+				return true;
+			}
+			else {
+				std::wcerr << L"--remove option requires <path>." << std::endl;
+			}
+		}
+
+		if ((arg == L"-p") || (arg == L"--paths")) {
+			input.first = arg;
+			return true;
+		}
+
+		if ((arg == L"-e") || (arg == L"--events")) {
+			input.first = arg;
+			return true;
+		}
+	}
+
+	ShowUsage(argv[0]);
+	return false;
+}
+
+/*
+* Helper function to display the application options.
+*/
+void ShowUsage(std::wstring appName) {
+	std::wcerr << "Usage: " << appName << " [option(s)]\n"
+		<< "Options:\n"
+		<< "\t-h,--help\t\tShow this help message\n"
+		<< "\t-a,--append <PATH>\tSpecify path to protect from\n"
+		<< "\t-r,--remove <PATH>\tSpecify path to remove from protection\n"
+		<< "\t-p,--paths \t\tShow all the defined paths\n"
+		<< "\t-e,--events \t\tShow event of blocked execution\n"
+		<< std::endl;
 }
