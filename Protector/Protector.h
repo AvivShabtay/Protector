5129c12e2f807c99309b2b62dca7d1408d16166c
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-03-28 11:22:25 +0300

    Remove old data types
    * note: Not using primitive linked-list anymore

--- Protector/Protector.h
+++ Protector/Protector.h
@@ -8,18 +8,6 @@
 #define DRIVER_PREFIX "Protector: "
 #define DRIVER_TAG 'Prtc'
 
-struct Globals {
-	LIST_ENTRY ItemsHead;
-	int ItemCount;
-	FastMutex Mutex;
-};
-
-template<typename T>
-struct FullItem {
-	LIST_ENTRY Entry;
-	T Data;
-};
-
 using ProtectorString = String<DRIVER_TAG, PagedPool>;
 using ProtectorVector = Vector<ProtectorString*, DRIVER_TAG>;
 
